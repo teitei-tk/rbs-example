2be3ab6fd9b3acaf57abe597cd7bd7002be8f259
teitei-tk <teitei.tk@gmail.com>
2023-01-19 02:33:31 +0900

    Initial commit

---
+++ lib/rbs/example.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "example/version"
+
+module Rbs
+  module Example
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rbs/example/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Rbs
+  module Example
+    VERSION = "0.1.0"
+  end
+end
